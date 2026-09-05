9bafaf9ab7cb7ab782ab66e0bbcaa982a9c9c611
shravankumarg361-dotcom <shravankumarg361@gmail.com>
2026-09-05 12:46:00 +0530

    Add new LeetCode solution

---
+++ MySQL/Easy/607. Sales Person.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT s.name
+FROM SalesPerson s
+WHERE s.name NOT IN
+(SELECT s.name FROM SalesPerson s
+LEFT JOIN Orders o on s.sales_id=o.sales_id
+LEFT JOIN Company c on o.com_id=c.com_id
+WHERE c.name='Red')
